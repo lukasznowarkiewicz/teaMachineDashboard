2bbd0f2bb507733875716514c8f618b7767a77d0
Szymon Nowarkiewicz <szynka2251@gmail.com>
2023-08-14 15:23:36 +0200

    Add new 'if else' to show selected frame, new functions and buttons configure. Change sticky of frame8button.grid to esw. Add new buttons with tea.

--- main.py
+++ main.py
@@ -67,46 +67,58 @@ class App(customtkinter.CTk):
 
         self.frame_4_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Sweetness",
                                                       fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
-                                                      image=self.add_user_image, anchor="w", command=self.frame_3_button_event)
+                                                      image=self.add_user_image, anchor="w", command=self.frame_4_button_event)
         self.frame_4_button.grid(row=4, column=0, sticky="ew")
 
         self.frame_5_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Solid additives",
                                                       fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
-                                                      image=self.add_user_image, anchor="w", command=self.frame_3_button_event)
+                                                      image=self.add_user_image, anchor="w", command=self.frame_5_button_event)
         self.frame_5_button.grid(row=5, column=0, sticky="ew")
 
         self.frame_6_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Liquid Additives",
                                                       fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
-                                                      image=self.add_user_image, anchor="w", command=self.frame_3_button_event)
+                                                      image=self.add_user_image, anchor="w", command=self.frame_6_button_event)
         self.frame_6_button.grid(row=6, column=0, sticky="ew")
 
         self.frame_7_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Done",
                                                       fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
-                                                      image=self.add_user_image, anchor="w", command=self.frame_3_button_event)
+                                                      image=self.add_user_image, anchor="w", command=self.frame_7_button_event)
         self.frame_7_button.grid(row=7, column=0, sticky="ew")
 
         self.frame_8_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Settings",
                                                       fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
-                                                      image=self.add_user_image, anchor="w", command=self.frame_3_button_event)
-        self.frame_8_button.grid(row=8, column=0, sticky="s")
+                                                      image=self.add_user_image, anchor="w", command=self.frame_8_button_event)
+        self.frame_8_button.grid(row=8, column=0, sticky="esw")
 
 
 
         # create home frame on the right - main window
         self.home_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
-        self.home_frame.grid(row=0, column=0, sticky="nsew")
-        self.home_frame.grid_columnconfigure(6, weight=1)
-        self.home_frame.grid_rowconfigure(3, weight=1)
-
+        self.home_frame.grid(row=2, column=5, sticky="nesw")
+        self.home_frame.columnconfigure(5, weight=1)
+        self.home_frame.rowconfigure(2, weight=1)
 
         self.home_frame_button_1 = customtkinter.CTkButton(self.home_frame, text="Black Tea", image=self.blackTeaImage, compound="top", font=("arial", 18), border_spacing=10)
-        self.home_frame_button_1.grid(row=1, column=0, padx=30, pady=20)
+        self.home_frame_button_1.grid(row=0, column=0, padx=30, pady=20)
         self.home_frame_button_2 = customtkinter.CTkButton(self.home_frame, text="Black Tea 2", image=self.blackTeaImage, compound="top", font=("arial", 18), border_spacing=10)
-        self.home_frame_button_2.grid(row=2, column=0, padx=30, pady=20)
-        self.home_frame_button_3 = customtkinter.CTkButton(self.home_frame, text="Black Tea 3", image=self.blackTeaImage, compound="top", font=("arial", 18), border_spacing=10)
-        self.home_frame_button_3.grid(row=1, column=1, padx=30, pady=20)
+        self.home_frame_button_2.grid(row=1, column=0, padx=30, pady=20)
         self.home_frame_button_4 = customtkinter.CTkButton(self.home_frame, text="Green Tea", image=self.blackTeaImage, compound="top", font=("arial", 18), border_spacing=10)
-        self.home_frame_button_4.grid(row=2, column=1, padx=30, pady=20)
+        self.home_frame_button_4.grid(row=0, column=1, padx=30, pady=20)
+        self.home_frame_button_5 = customtkinter.CTkButton(self.home_frame, text="Black Tea", image=self.blackTeaImage, compound="top", font=("arial", 18), border_spacing=10)
+        self.home_frame_button_5.grid(row=1, column=1, padx=30, pady=20)
+        self.home_frame_button_7 = customtkinter.CTkButton(self.home_frame, text="Black Tea 3", image=self.blackTeaImage, compound="top", font=("arial", 18), border_spacing=10)
+        self.home_frame_button_7.grid(row=0, column=2, padx=30, pady=20)
+        self.home_frame_button_8 = customtkinter.CTkButton(self.home_frame, text="Green Tea", image=self.blackTeaImage, compound="top", font=("arial", 18), border_spacing=10)
+        self.home_frame_button_8.grid(row=1, column=2, padx=30, pady=20)
+        self.home_frame_button_4 = customtkinter.CTkButton(self.home_frame, text="Green Tea", image=self.blackTeaImage, compound="top", font=("arial", 18), border_spacing=10)
+        self.home_frame_button_4.grid(row=0, column=3, padx=30, pady=20)
+        self.home_frame_button_5 = customtkinter.CTkButton(self.home_frame, text="Black Tea", image=self.blackTeaImage, compound="top", font=("arial", 18), border_spacing=10)
+        self.home_frame_button_5.grid(row=1, column=3, padx=30, pady=20)
+        self.home_frame_button_4 = customtkinter.CTkButton(self.home_frame, text="Green Tea", image=self.blackTeaImage, compound="top", font=("arial", 18), border_spacing=10)
+        self.home_frame_button_4.grid(row=0, column=4, padx=30, pady=20)
+        self.home_frame_button_5 = customtkinter.CTkButton(self.home_frame, text="Black Tea", image=self.blackTeaImage, compound="top", font=("arial", 18), border_spacing=10)
+        self.home_frame_button_5.grid(row=1, column=4, padx=30, pady=20)
+
 
         teaList = {"blackTea", "cherryTea", "greenTea"}
 
@@ -147,6 +159,12 @@ class App(customtkinter.CTk):
         self.home_button.configure(fg_color=("gray75", "gray25") if name == "home" else "transparent")
         self.frame_2_button.configure(fg_color=("gray75", "gray25") if name == "frame_2" else "transparent")
         self.frame_3_button.configure(fg_color=("gray75", "gray25") if name == "frame_3" else "transparent")
+        self.frame_4_button.configure(fg_color=("gray75", "gray25") if name == "frame_4" else "transparent")
+        self.frame_5_button.configure(fg_color=("gray75", "gray25") if name == "frame_5" else "transparent")
+        self.frame_6_button.configure(fg_color=("gray75", "gray25") if name == "frame_6" else "transparent")
+        self.frame_7_button.configure(fg_color=("gray75", "gray25") if name == "frame_7" else "transparent")
+        self.frame_8_button.configure(fg_color=("gray75", "gray25") if name == "frame_8" else "transparent")
+
 
         # show selected frame
         if name == "home":
@@ -161,6 +179,27 @@ class App(customtkinter.CTk):
             self.brewing_frame.grid(row=0, column=1, sticky="nsew")
         else:
             self.brewing_frame.grid_forget()
+        if name == "frame_4":
+            self.brewing_frame.grid(row=0, column=1, sticky="nsew")
+        else:
+            self.brewing_frame.grid_forget()
+        if name == "frame_5":
+            self.brewing_frame.grid(row=0, column=1, sticky="nsew")
+        else:
+            self.brewing_frame.grid_forget()
+        if name == "frame_6":
+            self.brewing_frame.grid(row=0, column=1, sticky="nsew")
+        else:
+            self.brewing_frame.grid_forget()
+        if name == "frame_7":
+            self.brewing_frame.grid(row=0, column=1, sticky="nsew")
+        else:
+            self.brewing_frame.grid_forget()
+        if name == "frame_8":
+            self.brewing_frame.grid(row=0, column=1, sticky="nsew")
+        else:
+            self.brewing_frame.grid_forget()
+        
 
     def toggle_fullscreen(self, event=None):
         is_fullscreen = self.attributes('-fullscreen')
@@ -175,6 +214,17 @@ class App(customtkinter.CTk):
 
     def frame_3_button_event(self):
         self.select_frame_by_name("frame_3")
+    
+    def frame_4_button_event(self):
+        self.select_frame_by_name("frame_4")    
+    def frame_5_button_event(self):
+        self.select_frame_by_name("frame_5")    
+    def frame_6_button_event(self):
+        self.select_frame_by_name("frame_6")    
+    def frame_7_button_event(self):
+        self.select_frame_by_name("frame_7")    
+    def frame_8_button_event(self):
+        self.select_frame_by_name("frame_8")
 
 
 if __name__ == "__main__":
